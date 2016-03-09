4146b188c19f24307e2075112013b1a256bc6f82
333250066 <333250066@C-C84PCX1.tdsb.on.ca>
2016-03-09 14:30:29 -0500

    Another test. No changes to functionality

--- src/scott/emmett/Grid.java
+++ src/scott/emmett/Grid.java
@@ -37,8 +37,7 @@ public class Grid {
 	public void printBoard(){
 		for(int i = 0; i < 6; i++){
 			for(int j = 0; j < 7; j++){
-				System.out.print(columns.get(j).get(i).getChar());
-				System.out.print(" ");
+				System.out.print(columns.get(j).get(i).getChar() + " ");
 			}
 			System.out.println("");
 		}
